c0c66317dc4db91d9cceb38fbdfdb720da948814
Tushar Paliwal <tushar.paliwal1997@gmail.com>
2018-09-14 13:35:59 +0530

    add more utility functions

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,13 @@
 public class Main {
     public static void main(String[] args) throws Exception {
         MyLinkedList ll = new MyLinkedList();
-        ll.addFirst(1);
-        ll.addLast(2);
-        ll.addLast(3);
-        ll.addFirst(10);
-        ll.addFirst(11);
-        ll.addFirst(12);
-        ll.display();
-        ll.reverseInGroup(3);
         ll.display();
         for(int i=10; i<20; i++) {
             ll.addLast(i);
         }
         ll.display();
-        ll.deleteMAfterN(2,0);
+        ll.reversePointerRecursive();
         ll.display();
+        //ll.deleteMAfterN(2,0);
     }
 }
--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -38,19 +38,53 @@ public class MyLinkedList {
         size++;
     }
 
-    public Node removeFirst() throws Exception {
-        if (isEmpty()) {
-            throw new Exception("List is already empty");
+    public void addAt(int data, int index) throws Exception {
+        if (index < 0 || index > this.getSize()) {
+            throw new Exception("Invalid index");
         }
-        Node removed = head;
-        if (this.getSize() == 1) {
-            this.head = null;
-            this.tail = null;
+
+        if (index == 0) {
+            this.addFirst(data);
+        } else if (index == this.size) {
+            this.addLast(data);
         } else {
-            this.head = this.head.next;
+            Node prevNode = this.getNodeAt(index - 1);
+            Node next = prevNode.next;
+            Node newNode = new Node(data, next);
+            prevNode.next = newNode;
+            this.size++;
         }
-        this.size--;
-        return removed;
+    }
+
+    public int getFirst() throws Exception {
+        if (this.isEmpty()) {
+            throw new Exception("List empty");
+        }
+
+        return this.head.data;
+    }
+
+
+    public int getLast() throws Exception {
+        if (this.isEmpty()) {
+            throw new Exception("List empty");
+        }
+
+        return this.tail.data;
+    }
+
+    public int getAt(int index) throws Exception {
+        if (index < 0 || index >= this.size) {
+            throw new Exception("Invalid index");
+        }
+
+        if (index == 0) {
+            return this.getFirst();
+        } else if (index == size - 1) {
+            return this.getLast();
+        } else {
+            return this.getNodeAt(index).data;
+        }
     }
 
     public Node removeLast() throws Exception {
@@ -71,8 +105,43 @@ public class MyLinkedList {
         return removed;
     }
 
+    public Node removeFirst() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("List is already empty");
+        }
+        Node removed = head;
+        if (this.getSize() == 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = this.head.next;
+        }
+        this.size--;
+        return removed;
+    }
+
+    public int removeAt(int index) throws Exception {
+        if (index < 0 || index >= this.size) {
+            throw new Exception("Invalid index");
+        }
+
+        if (index == 0) {
+            return removeFirst().data;
+        } else if (index == this.size - 1) {
+            return removeLast().data;
+        } else {
+            Node prev = this.getNodeAt(index - 1);
+            Node curr = prev.next;
+            Node next = curr.next;
+
+            prev.next = next;
+            size--;
+            return curr.data;
+        }
+    }
+
     public Node getNodeAt(int index) throws Exception {
-        if (index < 0 || index > this.getSize())
+        if (index < 0 || index >= this.getSize())
             throw new Exception("Invalid Index");
 
         Node returnNode = this.head;
@@ -86,11 +155,92 @@ public class MyLinkedList {
 
     public void display() {
         Node currentNode = this.head;
-        while(currentNode != null) {
+        while (currentNode != null) {
             System.out.print(currentNode.data + "->");
             currentNode = currentNode.next;
         }
         System.out.println("-------------------------");
+    }
+
+    public void reverseDataIterative() throws Exception {
+        int leftIndex = 0;
+        int rightIndex = this.getSize() - 1;
+
+        while (leftIndex <= rightIndex) {
+            Node left = this.getNodeAt(leftIndex);
+            Node right = this.getNodeAt(rightIndex);
+            int temp;
+            temp = left.data;
+            left.data = right.data;
+            right.data = temp;
+
+            leftIndex++;
+            rightIndex--;
+        }
+    }
+
+    public void reversePointerIterative() {
+        Node prev = this.head;
+        Node curr = prev.next;
+        while (curr != null) {
+            Node tempPrev = prev;
+            Node tempCurr = curr;
+
+            prev = curr;
+            curr = curr.next;
+            tempCurr.next = tempPrev;
+        }
+        Node temp = this.head;
+        this.head = this.tail;
+        this.tail = temp;
+    }
+
+    public void reversePointerRecursive() {
+        this.reversePointerRecursive(this.head);
+
+        Node temp = this.head;
+        this.head = this.tail;
+        this.tail = temp;
+
+        this.tail.next = null;
+    }
+
+    private void reversePointerRecursive(Node node) {
+        if (node == this.tail) {
+            return;
+        }
+        reversePointerRecursive(node.next);
+        node.next.next = node;
+    }
+
+    public void reverseDataRecursive() {
+        HeapMover left = new HeapMover(this.head);
+        this.reverseDataRecursive(left, this.head, 0);
+    }
+
+
+    private void reverseDataRecursive(HeapMover left, Node right, int floor) {
+        if (right == null) { //hit floor value, when right == null
+            return;
+        }
+        // recursive call shifting right by one node and increasing floor by 1
+        reverseDataRecursive(left, right.next, floor + 1);
+        if (floor >= this.getSize() / 2) { // Start Swapping left and right when in the middle of the list
+            int temp = left.node.data; // Swap
+            left.node.data = right.data; // Swap
+            right.data = temp; // Swap
+            left.node = left.node.next;
+        }
+    }
+
+
+    // Helper class for ReverseDataRecursive
+
+    private class HeapMover {
+        Node node;
+        public HeapMover(Node node) {
+            this.node = node;
+        }
     }
 
     public void reverseInGroup(int groupLength) throws Exception {
@@ -98,17 +248,17 @@ public class MyLinkedList {
             throw new Exception("List is too small");
         }
 
-        MyLinkedList finalList= new MyLinkedList();
+        MyLinkedList finalList = new MyLinkedList();
         MyLinkedList tempList = new MyLinkedList();
 
         Node currentNode = this.head;
-        while(currentNode != null) {
-            for (int i=0; i< groupLength; i++) {
+        while (currentNode != null) {
+            for (int i = 0; i < groupLength; i++) {
                 tempList.addFirst(currentNode.data);
                 currentNode = currentNode.next;
             }
 
-            if(finalList.isEmpty()) {
+            if (finalList.isEmpty()) {
                 finalList = tempList;
                 tempList = new MyLinkedList();
             } else {
@@ -122,30 +272,25 @@ public class MyLinkedList {
         this.tail.next = null;
     }
 
-    public void deleteMAfterN(int m, int n) throws Exception{
+    public void deleteMAfterN(int m, int n) throws Exception {
         // if the list is too small
-        if (m+n > this.getSize()) {
+        if (m + n > this.getSize()) {
             throw new Exception("List is too small");
         }
 
         Node nthNode = this.head;           // the node after which nodes will be deleted
         Node mthNode = this.head;           // the node until which nodes are to be deleted
-        for (int i=0; i<n+m; i++) {
-            if(i<n-1) {                     // increment nthNode n times
+        for (int i = 0; i < n + m; i++) {
+            if (i < n - 1) {                     // increment nthNode n times
                 nthNode = nthNode.next;
             }
-            mthNode = mthNode.next;         // increase mthNode (n + m) times
-//            if (i == n + m - 1) {           // set the next of nthNode to mthNode "removing"
-//                nthNode.next = mthNode;     // m nodes in the process
-//            } else if (n==0) {
-//                this.head = mthNode;
-//            }
+            mthNode = mthNode.next;
         }
-        if (n==0) {                             // if n=0 set head to mthNode
+        if (n == 0) {                             // if n=0 set head to mthNode
             this.head = mthNode;
         } else {
             nthNode.next = mthNode;             // set the next of nthNode to mthNode effectively
-    }                                           // "removing" m nodes in the process
+        }                                           // "removing" m nodes in the process
         this.size = this.getSize() - m;
     }                                       // fin
 
